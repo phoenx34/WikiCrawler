32c45b921d59a79b2ad93f94ee4219cbbc990936
phoenx34 <misaac34@iastate.edu>
2018-09-29 12:58:49 -0500

    -Started preliminary code for PriorityQ. -Created Data object for storing key and value

--- src/com/company/PriorityQ.java
+++ src/com/company/PriorityQ.java
@@ -1,43 +1,62 @@
 package com.company;
 
-abstract class Data implements Comparable<Data>{
-    private int key;
-    private String value;
-
-    public Data(int key, String value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    public int getKey() {
-        return this.key;
-    }
-
-    public String getValue() {
-        return this.value;
-    }
-
-    @Override
-    public int compareTo(Data data) {
-        if (data.getKey() >= this.getKey()) {
-            return data.getKey();
-        }
-
-        return this.getKey();
-    }
-}
 
 public class PriorityQ {
 
-    private int n;
+    private class Data implements Comparable<Data>{
+        private int key;
+        private String value;
 
-    public PriorityQ() {
+        public Data(int key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public int getKey() {
+            return this.key;
+        }
+
+        public String getValue() {
+            return this.value;
+        }
+
+        @Override
+        public int compareTo(Data data) {
+            if (data.getKey() >= this.getKey()) {
+                return data.getKey();
+            }
+
+            return this.getKey();
+        }
     }
 
-    public void add(String s, int p) {
+    private int n;
+    private Data[] data;
 
-//        heapify(s);
+    public PriorityQ() {
+        data = new Data[32];
+        data[0] = new Data(0, "");
+        n = 0;
+    }
+
+    public void add(Data d) {
+
+        Data temp = new Data(0, "");
+
+        if (n > 1) {
+            int kval = d.compareTo(data[n]);
+            if (kval == d.key) {
+                temp = data[n];
+                data[n] = d;
+                data[n + 1] = temp;
+            } else {
+
+            }
+
+        }
+//        heapify(data);
 //        assert isMaxHeap();
+
     }
 
     public String returnMax() {
